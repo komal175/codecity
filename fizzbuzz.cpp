27399f49d919138ffb54ff148db504d00d5fd5e6
komal175 <komal17500@gmail.com>
2024-10-28 23:07:20 +0630

    code

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -4,7 +4,7 @@ int main()
 {
     for(int i=1;i<=100;i++){
         if((i%3 == 0) && (i%5==0))
-            cout<<"FizzBuzz\n";
+            cout<<"FIZZBuzz\n";
         else if(i%3 == 0)
             cout<<"Fizz\n";
         else if(i%5 == 0)
