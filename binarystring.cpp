3b167e3139c01b446aeaff20cc23eccced0c034e
komal175 <komal17500@gmail.com>
2024-12-02 16:39:25 +0630

    binarystring

---
+++ binarystring.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >>t;
+    while(t--){
+        int n;
+        cin >>n;
+        string s;
+        cin >>s;
+        int decimal =stoi(s,nullptr,2);cout<<decimal<<endl;  
+    }
+}
