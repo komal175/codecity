97f2b26d4cd4d5dc864200b6dea354466ba089cd
komal175 <komal17500@gmail.com>
2024-12-06 23:34:14 +0630

    upper

--- lcm.cpp
+++ lcm.cpp
@@ -3,9 +3,13 @@ using namespace std;
 int main(){
     int a,b,c;
     cin>>a>>b>>c;
-    for(int i =1;i<=c;++i){
+    int cthnum;
+    for(int i=2;c>0;i++){
+        int i;
         if(i%a ==0 || i%b==0){
-            cout<<i;
+            c--;
+            cthnum =i;
         }
     }
 }
+    
