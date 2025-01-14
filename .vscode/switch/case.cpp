d6307797fcba54ab7bbe7cedb6ffa2971b18cc62
komal175 <komal17500@gmail.com>
2025-01-14 23:57:54 +0630

    array

---
+++ .vscode/switch/case.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int day;
+    cin >>day;
+    switch(day){
+        case 1:
+        cout<<"Monday";
+        case 2:
+        cout<<"Tuesday";
+        case 3:
+        cout<<"Wednesday";
+        case 4:
+        cout<<"Thrusday";
+        case 5:
+        cout<<"Friday";
+        case 6:
+        cout<<"Saturday";
+        case 7:
+        cout<<"Sunday";
+    }
+    cout<<"check";
+}
---
+++ array/2d.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[3][5];
+    arr[1][3] =7;
+    cout<<arr[1][2];
+    return 0;
+    }
