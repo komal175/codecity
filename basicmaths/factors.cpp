e236a0eb48eba4ff0eff38be2e7ead5b71603408
komal175 <komal17500@gmail.com>
2025-02-07 23:44:08 +0630

    _

--- basicmaths/factors.cpp
+++ basicmaths/factors.cpp
@@ -3,9 +3,12 @@ using namespace std;
 int main(){
     int a;
     cin>>a;
+    int sum =0;
     for(int i =1;i<=a;i++){
         if(a%i==0){
-            cout<<i<<endl;
+            cout<<i<<" ";
         }
+        sum += i;
+        cout<<sum;
     }
 }
