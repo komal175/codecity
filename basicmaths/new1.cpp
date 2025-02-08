f5d137e00bb0a22ea5f8f70d3389219f1e485e72
komal175 <komal17500@gmail.com>
2025-02-09 00:02:20 +0630

    palindrome

--- basicmaths/new1.cpp
+++ basicmaths/new1.cpp
@@ -3,4 +3,15 @@ using namespace std;
 int main(){
     int n;
     cin >>n;
-    
+    int reverse =0;
+    if(n<0){
+        cout<<"false";
+    }
+    long long int c =x;
+    int reverse =0;
+    while(c>0){
+        reverse = reverse%10 + c%10;
+    }
+    cout<<reverse;
+}
+
