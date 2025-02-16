f3b10cb638bf100d3181e19d9e4afd7f30353d9f
komal175 <komal17500@gmail.com>
2025-02-16 23:46:48 +0630

    _

--- pattern/starpyramid.cpp
+++ pattern/starpyramid.cpp
@@ -7,7 +7,7 @@ int main(){
         for(int j=1;j<=n-i-1;j++){
             cout<<" ";
         }
-        for(int k=0;k<2*i+1;k++){
+        for(int k=0;k<2*i+1;k++)|{
             cout<<"*";
         }
         for(int l=1;l<=n-i-1;l++){
