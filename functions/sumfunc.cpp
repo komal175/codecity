aefbb9a3661790765f6f031c9715e36550746db6
komal175 <komal17500@gmail.com>
2025-02-17 10:30:52 +0630

    recursion

---
+++ functions/sumfunc.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int a,int b){
+    int c = a+b;
+    cout<<c;
+}
+int main(){
+    int a,b;
+    cin >>a>>b;
+    sum(a,b);
+}
---
+++ jspwTest.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int happy(){
+    cout<<"happy"<<"\n";
+    happy();
+}
+int main(){
+    happy();
+    return 0;
+}
