99998834457b9c39a69dca6ec7613940a4dbb070
komal175 <komal17500@gmail.com>
2025-03-26 21:31:56 +0630

    _

--- basicmaths/lcm.cpp
+++ basicmaths/lcm.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main(){
     int a,b;
     cin >>a>>b;
-    int c = a*b;
+    int c = min(a,b);
     int f;
     for(int i =1;i<=c;i++){
         if(a%i==0 && b%i ==0){
             f = i;
         }
     }
-    int lcm = c/f;
+    int lcm = (a*b)/f;
     cout<<lcm;
 }
