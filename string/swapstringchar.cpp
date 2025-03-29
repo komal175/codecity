00bbe9e84ebe13e617ba79818d9bd1d64da6814f
komal175 <komal17500@gmail.com>
2025-03-29 22:39:14 +0630

    _

--- string/swapstringchar.cpp
+++ string/swapstringchar.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 	string a,b;
     cin >>a>>b;
-    cout <<a.size()<<" "<<b.size()<<endl;
+    cout<<" "<<b.size()<<endl;
     string c = a+b;
     cout <<c<<endl;
     swap(a[0],b[0]);
