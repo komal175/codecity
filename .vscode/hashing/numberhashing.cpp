422e086d4da6bbe0b2ecb9ad2fc4c2c2c3e6849e
komal175 <komal17500@gmail.com>
2025-04-14 10:19:58 +0630

    _

--- .vscode/hashing/numberhashing.cpp
+++ .vscode/hashing/numberhashing.cpp
@@ -1,25 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int n;
     cin>>n;
-    int a[n];
+    vector<int> a(n);
     for(int i=0;i<n;i++){
         cin>>a[i];
+        cout<<a[i]<<" ";
     }
-    //prestoring
-    int hash[n]={0};
-    for(int i =0;i<n;i++){
-        hash[a[i]]+=1;
+    cout<<"\n";
+    //precompute
+    int hash[13]={0};
+    for(int i=0;i<13;i++){
+        cout<<hash[i]<<" ";
     }
-    int q;
-    cin>>q;
-    while(q--){
-        int number;
-        cin>>number;
-    
+    for(int i=0;i<n;i++){
+        hash[a[i]] +=1;
+    }
     //fetching
-    cout<<hash[number]<<endl;
+    int t;
+    cin>>t;
+    while(t--){
+        int num;
+        cin>>num;
+        cout<<"\n"<<hash[num];
     }
-    return 0;
 }
