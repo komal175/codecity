f9916687cbd89d695c0e675d14c889e134b606ea
komal175 <komal17500@gmail.com>
2025-05-12 22:45:15 +0630

    _

--- .vscode/arrays/spiral.cpp
+++ .vscode/arrays/spiral.cpp
@@ -1,17 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    vector<int> nums(n);
-        map<long long int,int> mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it:mpp){
-            if(it.second > nums.size()/2){
-                return it.first;
-            }
-        }
-        return -1;
-}
+
