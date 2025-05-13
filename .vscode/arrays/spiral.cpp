b86cf64751923956a98297942ed1df907d076814
komal175 <komal17500@gmail.com>
2025-05-13 22:46:54 +0630

    _

--- .vscode/arrays/spiral.cpp
+++ .vscode/arrays/spiral.cpp
@@ -1,1 +1,18 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int xor2 =0;
+    int xor1=0;
+    for(int j=0;j<n;j++){
+        xor2=xor2^nums[j];
+        xor1 =xor1^(j+1);
+    }
+    int ans=xor1 ^ xor2;
+    cout<<ans;
+}
