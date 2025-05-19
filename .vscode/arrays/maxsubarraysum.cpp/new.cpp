bc41c78f3026e9f461f8b4d2fae983f79a5d8ce1
komal175 <komal17500@gmail.com>
2025-05-19 23:50:53 +0630

    _

--- .vscode/arrays/maxsubarraysum.cpp/new.cpp
+++ .vscode/arrays/maxsubarraysum.cpp/new.cpp
@@ -1,37 +1,29 @@
-#include <bits/stdc++.h>
+//kadanes algorithm
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
+int main(){
     int n;
-    cin >> n;
+    cin>>n;
     vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
     }
-
-    int sum = 0;
-    int maxi = INT_MIN;
-    int start = 0;
-    int ansstart = -1, ansend = -1;
-
-    for (int i = 0; i < n; i++) {
-        if (sum < 0) {
-            sum = 0;
-            start = i;
+    int sum=0;
+    int maxi=INT_MIN;
+    int ansstart=-1,ansend=-1,start=0;
+    for(int i=0;i<n;i++){
+        if(sum<0){
+            sum=0;
+            start=i;
         }
-        sum += nums[i];
-        if (sum > maxi) {
-            maxi = sum;
-            ansstart = start;
-            ansend = i;
+        sum+=nums[i];
+        if(sum>maxi){
+            maxi=max(maxi,sum);
+            ansstart=start;
+            ansend=i;
         }
     }
-
-    // Output the maximum subarray
-    for (int i = ansstart; i <= ansend; i++) {
-        cout << nums[i] << " ";
+    for(int i=ansstart;i<=ansend;i++){
+        cout<<nums[i]<<" ";
     }
-    cout << "\nMax Sum: " << maxi << endl;
-
-    return 0;
 }
